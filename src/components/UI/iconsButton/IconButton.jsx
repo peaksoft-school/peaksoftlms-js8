60fb923b7edba356155230445987984c9a27ad39
bekbol <kanybekuulubekbol26@gamil.comgit config --global user.email kanybekuulubekbol26@gmail.com>
2023-04-05 15:54:50 +0600

    iconButton done

---
+++ src/components/UI/iconsButton/IconButton.jsx
@@ -0,0 +1,8 @@
+import { IconButton } from '@mui/material'
+import React from 'react'
+
+const IconButtons = ({ children }) => {
+   return <IconButton>{children}</IconButton>
+}
+
+export default IconButtons
