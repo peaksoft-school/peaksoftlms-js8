c4efe2404aa240e35e407e998ff001ef0b178ef3
mirlnovna <120446226+mirlnovna@users.noreply.github.com>
2023-04-06 12:19:42 +0600

    input

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -3,14 +3,12 @@ import TextField from '@mui/material/TextField'
 
 function FullWidthTextField({ variant, placeholder, value, ...rest }) {
    return (
-      <div>
-         <TextField
-            variant={variant}
-            placeholder={placeholder}
-            value={value}
-            {...rest}
-         />
-      </div>
+      <TextField
+         variant={variant}
+         placeholder={placeholder}
+         value={value}
+         {...rest}
+      />
    )
 }
 export default FullWidthTextField
