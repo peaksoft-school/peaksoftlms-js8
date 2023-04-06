b04e937fea3fb6331b3a6d6e896a3556917ed084
Zarina <122347289+ZarinaOrgubaeva@users.noreply.github.com>
2023-04-06 13:31:45 +0600

    updated theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ThemeProvider } from '@mui/material'
-import { theme } from './components/UI/theme'
+import { theme } from './utlis/constants/theme'
 
 function App() {
    return <ThemeProvider theme={theme}>Peaksoft LMS</ThemeProvider>
