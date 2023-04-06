88bf90c9636d478f67115124cdb85ee76177249c
bekbol <kanybekuulubekbol26@gamil.comgit config --global user.email kanybekuulubekbol26@gmail.com>
2023-04-06 14:35:16 +0600

    iconButton fixed

---
+++ src/components/UI/IconButton.jsx
@@ -0,0 +1,8 @@
+import { IconButton as MuiIconButton } from '@mui/material'
+import React from 'react'
+
+const IconButton = ({ icon }) => {
+   return <MuiIconButton>{icon}</MuiIconButton>
+}
+
+export default IconButton
--- src/components/UI/iconsButton/IconButton.jsx
+++
@@ -1,8 +0,0 @@
-import { IconButton } from '@mui/material'
-import React from 'react'
-
-const IconButtons = ({ children }) => {
-   return <IconButton>{children}</IconButton>
-}
-
-export default IconButtons
