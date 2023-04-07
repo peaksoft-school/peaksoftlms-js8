d5097c6d6305a310099243e0a6fdbd0641695bab
Omina <omina.mamatalieva.ch@gmail.com>
2023-04-07 19:21:33 +0600

    change Card

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -5,15 +5,15 @@ import CardContent from '@mui/material/CardContent'
 import CardActions from '@mui/material/CardActions'
 import { ReactComponent as Icons } from '../../assets/icons/Frame.svg'
 
-const Cards = ({ title, content, image, data }) => {
+const Cards = ({ title, content, image, date }) => {
    return (
       <CardStyled>
          <CardMedia component="img" alt="projects" height="171" image={image} />
          <DivStyled>
             <CardContent>{title}</CardContent>
-            <CardContent>{data}</CardContent>
+            <CardContent>{date}</CardContent>
          </DivStyled>
-         <CardContent>{content}</CardContent>
+         <StyledCartContent>{content}</StyledCartContent>
          <CardActionStyled>
             <Icons />
          </CardActionStyled>
@@ -25,15 +25,34 @@ export default Cards
 
 const DivStyled = styled.div({
    display: 'flex',
+   maxWidth: 234,
+   height: 18,
+   marginLeft: 18,
+   marginRight: 18,
+   marginTop: 16,
 })
 
 const CardStyled = styled(Card)({
    maxWidth: 270,
-   height: 311,
+   height: 350,
    borderRadius: 10,
 })
 
 const CardActionStyled = styled(CardActions)({
-   marginLeft: 230,
-   marginTop: 30,
+   marginLeft: 228,
+   marginBottom: 8,
 })
+
+const StyledCartContent = styled(CardContent)`
+   display: flex;
+   margin-left: 18px;
+   margin-right: 18px;
+   margin-top: 10px;
+   .MuiTypography-root {
+      white-space: pre-wrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      width: 234;
+      height: 66px;
+   }
+`
