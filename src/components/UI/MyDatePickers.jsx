8ce1f92691fd9f0447c5fe08dab9a2024098d14c
Omina <omina.mamatalieva.ch@gmail.com>
2023-04-12 19:59:08 +0600

    added Date Pickers

---
+++ src/components/UI/MyDatePickers.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import useDatePicker from '../../hooks/datePickers'
+
+function MyDatePickers() {
+   const { date, handleDateChange } = useDatePicker()
+
+   return (
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+         <DemoContainer components={['DatePicker', 'DatePicker']}>
+            <DatePicker
+               value={date}
+               onChange={(newValue) => handleDateChange(newValue)}
+            />
+         </DemoContainer>
+      </LocalizationProvider>
+   )
+}
+
+export default MyDatePickers
