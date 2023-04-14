845c9fc11184d40cddb90f3cc7bec74a6db7ee06
bekbol <kanybekuulubekbol26@gamil.comgit config --global user.email kanybekuulubekbol26@gmail.com>
2023-04-14 12:17:57 +0600

    Switcher

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react'
+import { SwitchesSize } from './components/UI/AcceptBar'
+
 function App() {
-   return <div>PeaksoftLMS</div>
+   const [isChecked, setChecked] = useState(true)
+
+   const CheckHandled = () => {
+      setChecked(!isChecked)
+   }
+   return (
+      <div>
+         PeaksoftLMS
+         <SwitchesSize onChange={CheckHandled} checked={isChecked} />
+      </div>
+   )
 }
 
 export default App
