b8433b39a1a1371beea9c8ee4f214c23c5f79a83
mirlnovna <aidaimakhmatova@gmail.com>
2023-04-17 16:36:31 +0600

    fixTextArea

--- src/components/UI/TextArea.jsx
+++ src/components/UI/TextArea.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import styled from '@emotion/styled'
 import Input from './Input'
 
-const TextArea = ({ value, onChange, ...rest }) => {
+const TextArea = forwardRef(({ value, onChange, ...rest }, ref) => {
    return (
       <StyledInput
+         ref={ref}
          multiline
          rows={3}
          placeholder="Oписание курса"
@@ -13,11 +14,10 @@ const TextArea = ({ value, onChange, ...rest }) => {
          {...rest}
       />
    )
-}
+})
 
 export default TextArea
 
 const StyledInput = styled(Input)({
-   width: '491px',
    borderRadius: '10px',
 })
