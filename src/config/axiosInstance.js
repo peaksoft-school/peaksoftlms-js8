7d4f3d790e3d48306b390a5a64b8e25b7a47afa9
Zarina <122347289+ZarinaOrgubaeva@users.noreply.github.com>
2023-04-26 12:42:47 +0600

    axios-Completed

---
+++ src/config/axiosInstance.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const BASE_URL = 'dkjdnjd'
+const store = []
+
+export const axiosInstance = axios.create({
+   baseURL: BASE_URL,
+})
+
+axiosInstance.interceptors.request.use(
+   function (config) {
+      const newConfig = {
+         ...config,
+         headers: {
+            ...config.headers,
+            Authorization: store.getState().auth.token,
+         },
+      }
+      return newConfig
+   },
+   function (error) {
+      return Promise.reject(error)
+   }
+)
+axiosInstance.interceptors.response.use(
+   function (response) {
+      if (response.status === 401) {
+         store.dispatch()
+      }
+      return response
+   },
+   function (error) {
+      return Promise.reject(error)
+   }
+)
